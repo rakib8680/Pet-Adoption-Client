69e54f3208bf8d78e699d8d1ef7e218a032101fb
rakib8680 <rakib.cst.1st@gmail.com>
2024-05-25 20:39:15 +0600

    Initialized Next Js app with mui

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@mui/material";
+
+
+
+
+const HomePage = () => {
+  return (
+     <div>
+         <h1>This is HomePage component</h1>
+         <Button variant="contained">Contained</Button>
+     </div>
+  )
+};
+
+export default HomePage;

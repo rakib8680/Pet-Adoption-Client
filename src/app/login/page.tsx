70e708c475c41c7633a49624255ec965ee3ec777
rakib8680 <rakib.cst.1st@gmail.com>
2024-05-27 21:25:49 +0600

    Login Form Design Complete

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,116 @@
+"use client";
 
-
-
-
+import Lottie from "lottie-react";
+import login_animation2 from "@/assets/login_animation2.json";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import Image from "next/image";
+import logo2 from "@/assets/logo2.png";
+import Link from "next/link";
 
 const LoginPage = () => {
   return (
-     <div>
-         <h1>This is LoginPage component</h1>
-     </div>
-  )
+    <div
+      className="
+    h-screen bg-gradient-to-br from-[#fffded] to-[#fff4f4] flex flex-col justify-between
+    "
+    >
+      {/* logo   */}
+      <Stack
+        direction="row"
+        alignItems="center"
+        gap={2}
+        component={Link}
+        p={4}
+        href="/"
+      >
+        <Image
+          src={logo2}
+          alt="logo"
+          quality={100}
+          width={50}
+          className="rounded-full"
+        />
+        <Typography fontWeight={600} fontSize={23}>
+          <Box component="span" color="accent.main" className="font-black ">
+            Pet
+          </Box>{" "}
+          Adoption Center 🐾
+        </Typography>
+      </Stack>
+
+      <div
+        className=" container  mx-auto flex justify-center items-center h-[90vh] gap-60
+      "
+      >
+        <Lottie
+          animationData={login_animation2}
+          loop={true}
+          className="w-[450px]"
+        />
+
+        <Box
+          sx={{
+            border: "1px solid #B6B9B8",
+            padding: "50px",
+            borderRadius: "10px",
+            bgcolor: "#fff",
+          }}
+        >
+          <Stack
+            spacing={2}
+            sx={{
+              width: "350px",
+              marginRight: "100px",
+              margin: "0 auto",
+            }}
+          >
+            <Image
+              src={logo2}
+              alt="logo"
+              width={70}
+              style={{
+                borderRadius: "50%",
+                border: "1px solid #ccc",
+                padding: "5px",
+                margin: "0 auto",
+              }}
+            />
+            <Typography
+              variant="h4"
+              color="primary"
+              align="center"
+              fontWeight="600"
+              fontSize="30px"
+              className="font-bold"
+            >
+              Sign In
+            </Typography>
+            <TextField label="Enter your email" variant="outlined" />
+            <TextField label="Password" type="password" variant="outlined" />
+            <Button disableElevation color="primary">
+              Login
+            </Button>
+            <Typography
+              align="center"
+              color="accent.main"
+              fontSize="14px"
+              fontWeight={100}
+            >
+              Don&apos;t have an account?{" "}
+              <Box
+                color="secondary.main"
+                component={Link}
+                href="/register"
+                className="hover:underline"
+              >
+                SignUp
+              </Box>
+            </Typography>
+          </Stack>
+        </Box>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
---
+++ src/components/Forms/PAC_Form.tsx
@@ -0,0 +1,10 @@
+
+const PAC_Form = () => {
+  return (
+     <div>
+         <h1>This is PAC_Form component</h1>
+     </div>
+  )
+};
+
+export default PAC_Form;
